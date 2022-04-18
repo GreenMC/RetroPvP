0b59a472cf75bbe435985a83ce32e5074f5e4966
Ali Arslan <aliarslann5500@gmail.com>
2022-04-18 21:15:24 +0300

    No more rain

--- src/main/java/io/github/greenmc/retropvp/listeners/ServerListener.java
+++ src/main/java/io/github/greenmc/retropvp/listeners/ServerListener.java
@@ -4,6 +4,7 @@ import io.github.greenmc.retropvp.RetroPvP;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class ServerListener extends ListenerAdapter {
 
@@ -14,6 +15,11 @@ public class ServerListener extends ListenerAdapter {
 	@EventHandler
 	public void onSpawn(EntitySpawnEvent e) {
 		if (e.getEntity() instanceof Monster) e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onWeatherChange(WeatherChangeEvent e) {
+		e.setCancelled(true);
 	}
 
 }
