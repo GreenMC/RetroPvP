3a48793761a8b8733e74e9590ea8d0b3467fb7c0
Ali Arslan <aliarslann5500@gmail.com>
2022-04-19 11:52:20 +0300

    Add regen testing

--- src/main/java/io/github/greenmc/retropvp/listeners/PlayerListener.java
+++ src/main/java/io/github/greenmc/retropvp/listeners/PlayerListener.java
@@ -1,20 +1,72 @@
 package io.github.greenmc.retropvp.listeners;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import io.github.greenmc.retropvp.RetroPvP;
 import io.github.greenmc.retropvp.api.StatsStorage;
 import io.github.greenmc.retropvp.user.User;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.Base64;
+import java.util.UUID;
 
 public class PlayerListener extends ListenerAdapter {
 
+	private static ItemStack skull;
+
     public PlayerListener(RetroPvP plugin) {
         super(plugin);
+		prepareSkull();
     }
+
+	private void prepareSkull() {
+		skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+		SkullMeta meta = (SkullMeta) skull.getItemMeta();
+
+		GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+		byte[] encodedData = Base64.getEncoder().encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", "https://textures.minecraft.net/texture/e6a106bd7c5bd3fd06d908dff1c77325c521dc8735c01aade777f056421ad929").getBytes());
+		profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+		Field profileField;
+		try
+		{
+			profileField = meta.getClass().getDeclaredField("profile");
+			profileField.setAccessible(true);
+			profileField.set(meta, profile);
+		}
+		catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}
+		skull.setItemMeta(meta);
+	}
+
+	// TODO: REMOVE THIS, JUST FOR TESTING PURPOSES
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent e) {
+		if (e.getMessage().equals("kafa")) {
+			ItemStack item = skull.clone();
+			ItemMeta meta = item.getItemMeta();
+			meta.setDisplayName("§a§lCan Kiti");
+			item.setItemMeta(meta);
+			plugin.getServer().getScheduler().runTask(plugin, () -> {
+				Item entity = e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), item);
+				entity.setPickupDelay(Integer.MAX_VALUE);
+			});
+			e.getPlayer().getInventory().addItem(item);
+		}
+	}
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
